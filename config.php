8573cf1521fc7c0fe2bbaff1198d7341038f069d
[.::MDT::.] <mdt86@yahoo.it>
2013-07-01 17:23:57 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+ 
+/**
+ * My config
+ *
+ * To use this config simply do the following:
+ *
+ *     $config = include('config.php');
+ */
+ 
+return array(
+    'config' => array(
+        'APIkey' => 'INSERT YOUR TRAKT API KEY HERE',
+        'user' => 'INSERT YOUR TRAKT USER NAME HERE',
+        'pass' => 'INSERT YOUR TRAKT PASSWORD HERE',
+    )
+);
