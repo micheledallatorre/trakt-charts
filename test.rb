c29b1487b521da71a9dd4e6050137b66a5a72fb7
[.::MDT::.] <mdt86@yahoo.it>
2013-07-09 17:14:33 +0200

    Update test.rb
    Code cleaned up

--- test.rb
+++ test.rb
@@ -6,33 +6,31 @@ require 'open-uri'
 require 'pp'
 
 
-importfile = 'lista.csv'
-myfile = File.open(importfile)	
-films = {}
-mylist = {}
-counter = 0
-imdblist = File.open('imdblist.csv')	
 
-# print  e.g. 
-# 127 Hours;2010;60%;19/3/2011;1300489200
-def printFile(mtitle, myear, rating, seendate, mytimestamp)
-	puts "#{mtitle};#{myear};#{rating};#{seendate};#{mytimestamp}"
+#create file
+def createFile(myfile) 
+	file = File.open(myfile, "w+")
+	return file
+end
+
+#print to file
+def appendToFile(myfile, text)
+	file = File.open(myfile, "a")
+	file.write(text) 
 end
 
 # outputs all data taken from the http://www.omdbapi.com/ website 
 # e.g. http://www.omdbapi.com/?t=127%20Hours&y=2010
-def findIMDBIDviaURI(myurllist)
-	myurllist.each do |title,url|
-		#puts title + "-------------" + url
+def findIMDBIDviaURI(list)
+	list.each do |key,elem|
+		url = elem['omdbapiurl']
 		myuri = URI.parse(URI.encode(url.strip))
-		#p myuri
 
 		open(myuri) { |f|
 			f.each_line { |line| 
 				p line
 			}
 		}
-
 	end
 end
 
@@ -43,146 +41,22 @@ def printArray(arr)
 	end
 end 
 
-#read input file and store data into films hash
-File.foreach(importfile) do |line|
-	#split line on ";" into the 3 fields below
-	#E.g. a line is as follows
-	# 127 Hours (2010);60%;3/19/11
-	name,rating,date = line.split(';')
-	# create films hash 
-	films[name] = rating, date
-end
- 
-#for each movie into films hash 
-films.each_pair{ |key,value| 
-	#get movie title without year
-	#e.g. from "127 Hours (2010)" to "127 Hours"
-	movietitle = key[/(.*[^([0-9]{4})])/].strip! 
-	#get movie year
-	movieyear = key[/(.*\(([0-9]{4})\))/,2]
-	g1 = key[/(.+)\s+\(([0-9]{4})\)/,1]
-	g2 = key[/(.+)\s+\(([0-9]{4})\)/,2]	
-	# transform rating from e.g 80% to 8
-	myrating = value[0][0, value[0].length - 2]
-
-	
-	# format: 3/19/11
-	seendate = value[1]
-	#get month, day and year  by splitting the string on "/"
-	# convert them to INTEGER
-	seenmonth = seendate[/(\d+)\/(\d+)\/(\d+)/,1].to_i
-	seenday = seendate[/(\d+)\/(\d+)\/(\d+)/,2].to_i
-	#year: add "20" to get from e.g. "11" to "2011" (o.w. it's "0011"!!!)
-	seenyear = ("20" + seendate[/(\d+)\/(\d+)\/(\d+)/,3]).to_i
-	
-	# convert into a Date object
-	mydate = Date.new(seenyear,seenmonth,seenday)
-	# convert into a Time object
-	mytime = Time.new(seenyear, seenmonth, seenday, 0, 0, 0,)
-	# convert into a timestamp
-	mytimestamp = mytime.to_i
-
-	myformattedseendate = seenday.to_s + "/" + seenmonth.to_s + "/" + seenyear.to_s
-	#printFile(g1, g2, myrating, myformattedseendate, mytimestamp)
-	
-	mytmplist = {}
-	mytmplist['title'] = movietitle
-	mytmplist['year'] = movieyear	
-	mytmplist['plays'] = 1
-	mytmplist['last_played'] = mytimestamp
-	
-	mytmpRatings = {}
-	mytmpRatings['imdb_id'] = ""
-	mytmpRatings['title'] = movietitle
-	mytmpRatings['year'] = movieyear	
-	mytmpRatings['rating'] = myrating
-
-	#CHOOSE which list to use!
-	mylist[counter] = mytmpRatings
-	counter = counter+1
-}
-
-myurlarray = []
-moviestitles = []
-myurllist = {}
-myres = []
-#print all titles-years with links
-mylist.each { |key,value| 
-	myurlarray.push "http://www.omdbapi.com/?t=#{value['title']}&y=#{value['year']}"
-	
-	x = [value['title'], value['year'], ""]
-	moviestitles.push x
-	
-	myurllist[value['title']] = "http://www.omdbapi.com/?t=#{value['title']}&y=#{value['year']}"
-}
-
-#printArray(myurlarray)
- 
-# call function to get IMDBID via URL API
-#findIMDBIDviaURI(myurllist)
-
-#print mylist.to_json
- 
- temp = []
- templist = {}
- index = 0
- #read file
- #"{\"Title\":\"127 Hours\",\"Year\":\
- File.foreach(imdblist) do |line|
-	# get title name
-	title = line[/Title.{5}([^\\]*).*/,1]
-	
-	# if title is null or empty (i.e. movie was not found)
-	if title == "" or title == nil
-		title = "NOTFOUND"
+#prints only NOT FOUND imdbid for movies
+def printNotFoundIMDBIDs(list)
+	counter = 0
+	puts "List of movies whose IMDB ID was NOT found\n"
+	list.each do |key,elem|
+		imdbid = elem['imdb_id']
+		if imdbid == "" or imdbid == nil
+			puts "\t" + key 
+			counter += 1;
+		end
 	end
-	
-	imdbid = line[/imdbID.{5}([^\\]*).*/,1]
-	if imdbid == "" or imdbid == nil
-		imdbid = "NOTFOUND"
-	end
-	
-	moviestitles[index][2] = imdbid
-	index += 1
-	
-	temp.push(title)
-	temp.push(imdbid)
-	templist[title]= imdbid
-	
-  end
-  
-#pp moviestitles
-
-#print only NOT FOUND imdbid for movies
-=begin
-moviestitles.each do |a|
-	if a[2] == "NOTFOUND"
-		pp a
-	end
+	puts "\nIMDB ID not found for " + counter.to_s + " movies out of " + list.length.to_s + "!"
 end 
-=end
+
 
 #print movies JSON for import into trakt
-puts "\"movies\": ["
-moviestitles.each do |a|
-	puts "\t{"
-	
-	if a[2] == "NOTFOUND"
-		puts "\t}"
-	else 
-		puts "\t\t\"imdb_id\": \"" + a[2] + "\","
-	end
-	
-	puts "\t\t\"title\": \"" + a[0] + "\","
-	puts "\t\t\"year\": " + a[1] + ","	
-	puts "\t}"
-	 
-end 
-puts "]"
- 
-#printArray(temp)
-#pp templist
- 
 =begin
 TO POST SEEN MOVIE TO TRAKT:
 {
@@ -198,7 +72,30 @@ TO POST SEEN MOVIE TO TRAKT:
         }
     ]
 }
+=end
+def printJSONseenMovie (list)
+	length = list.length
+	counter = 0
+	puts "\"movies\": ["
+	list.each do |k,elem|
+		puts "\t{"
+			puts "\t\t\"imdb_id\": \"" + elem['imdb_id']  + "\","
+			puts "\t\t\"title\": \"" + k + "\","
+			puts "\t\t\"year\": " + elem['year'] + ","	
+			puts "\t\t\"plays\": " + elem['plays'].to_s + ","
+			puts "\t\t\"last_played\": " + elem['last_played'].to_s
+		counter += 1			
+		if counter == length 
+			puts "\t}"	 
+		else
+			puts "\t},"
+		end
+	end 
+	puts "]"
+end
 
+#print movies JSON for import into trakt
+=begin
 TO POST MOVIE RATINGS TO TRAKT
 http://trakt.tv/api-docs/rate-movies
 {
@@ -220,6 +117,169 @@ http://trakt.tv/api-docs/rate-movies
     ]
 }
 =end
+def printJSONrateMovie (list)
+	length = list.length
+	counter = 0
+	puts "\"movies\": ["
+	list.each do |k,elem|
+		puts "\t{"
+			puts "\t\t\"imdb_id\": \"" + elem['imdb_id']  + "\","
+			puts "\t\t\"title\": \"" + k + "\","
+			puts "\t\t\"year\": " + elem['year'] + ","	
+			puts "\t\t\"rating\": " + elem['rating'].to_s
+		counter += 1			
+		if counter == length 
+			puts "\t}"	 
+		else
+			puts "\t},"
+		end
+	end 
+	puts "]"
+end
+
+
+importfile = 'lista.csv'
+myfile = File.open(importfile)	
+# generated from input importfile
+# format:
+# movietitle (movieyear) => rating, date
+# E.g. "127 Hours (2010)"=>["60%", "3/19/11\n"],
+inputfilms = {}
+# contains all the data about films
+=begin
+FORMAT:
+{"127 Hours"=>
+  {"title"=>"127 Hours",
+   "year"=>"2010",
+   "plays"=>1,
+   "last_played"=>1300489200,
+   "imdb_id"=>"",
+   "rating"=>"6"},
+   ...
+ }
+=end
+filmsdata = {}
+counter = 0
+imdblist = File.open('imdblist.csv')	
+
+
+
+
+
+
+#read input file and store RAW data into films hash
+File.foreach(importfile) do |line|
+	#split line on ";" into the 3 fields below
+	#E.g. a line is as follows
+	# 127 Hours (2010);60%;3/19/11
+	name,rating,date = line.split(';')
+	# create films hash 
+	inputfilms[name] = rating, date
+end
+ 
+#for each movie into inputfilms hash 
+# parse and create data for FILMSDATA list
+inputfilms.each_pair{ |key,value| 
+	#get movie title without year
+	#e.g. from "127 Hours (2010)" to "127 Hours"
+	movietitle = key[/(.*[^([0-9]{4})])/].strip! 
+	#get movie year
+	movieyear = key[/(.*\(([0-9]{4})\))/,2]
+	# transform rating from e.g 80% to 8
+	myrating = value[0][0, value[0].length - 2]
+
+	
+	# format: 3/19/11
+	seendate = value[1]
+	#get month, day and year  by splitting the string on "/"
+	# convert them to INTEGER
+	seenmonth = seendate[/(\d+)\/(\d+)\/(\d+)/,1].to_i
+	seenday = seendate[/(\d+)\/(\d+)\/(\d+)/,2].to_i
+	#year: add "20" to get from e.g. "11" to "2011" (o.w. it's "0011"!!!)
+	seenyear = ("20" + seendate[/(\d+)\/(\d+)\/(\d+)/,3]).to_i
+	
+	# convert into a Date object
+	mydate = Date.new(seenyear,seenmonth,seenday)
+	# convert into a Time object
+	mytime = Time.new(seenyear, seenmonth, seenday, 0, 0, 0,)
+	# convert into a timestamp
+	mytimestamp = mytime.to_i
+
+	myformattedseendate = seenday.to_s + "/" + seenmonth.to_s + "/" + seenyear.to_s
+		
+	# temporary list to be added to FILMSDATA list
+	mytmplist = {}
+	mytmplist['title'] = movietitle
+	mytmplist['year'] = movieyear	
+	mytmplist['plays'] = 1
+	mytmplist['last_played'] = mytimestamp
+	mytmplist['seen_date'] = myformattedseendate
+	mytmplist['imdb_id'] = ""
+	mytmplist['rating'] = myrating
+	mytmplist['omdbapiurl'] = ""
+	# add info to FILMSDATA list
+	filmsdata[movietitle] = mytmplist
+
+}
+
+
+
+#add to FILMSDATA the url for OMBDAPI to get info about the imdbID
+filmsdata.each { |key,value| 
+	filmsdata[key]['omdbapiurl'] = "http://www.omdbapi.com/?t=#{value['title']}&y=#{value['year']}"
+}
+ 
+# call function to get IMDBID via URL API
+#findIMDBIDviaURI(filmsdata)
+
+#print filmsdata.to_json
+ 
+ # Read file from OMDBAPI calling, in the format below, and parse data into FILMSDATA
+ #"{\"Title\":\"127 Hours\",\"Year\":\
+ File.foreach(imdblist) do |line|
+	# get title name from file
+	title = line[/Title.{5}([^\\]*).*/,1]
+	#get IMDBID from file
+	imdbid = line[/imdbID.{5}([^\\]*).*/,1]
+	
+=begin
+	# if title is null or empty (i.e. movie was not found)
+	if title == "" or title == nil
+		title = "NOTFOUND"
+	end
+	
+
+	if imdbid == "" or imdbid == nil
+		imdbid = "NOTFOUND"
+	end
+=end
+		
+	#EXACT MATCH TO AVOID ERRORS!
+	#add IMDBID to FILMSDATA matching the title
+	if filmsdata.has_key?(title)
+		filmsdata[title]['imdb_id'] = imdbid
+	#else
+	#	puts "filmdata title:" + filmsdata['title'].to_s + "; found title:" + title.to_s
+	end
+ end
+
+
+=begin
+# create backup file with all my FILMSDATA in CSV format
+myoutfile = createFile("backup.txt");
+filmsdata.each do |k,v|
+	# separator is comma
+	sep = ","
+	line = 	k + sep 			+ v['year'] + sep 			+ v['plays'].to_s + sep 			+ v['last_played'].to_s + sep 			+ v['seen_date'].to_s + sep 			+ v['imdb_id'].to_s + sep			+ v['rating'].to_s + sep 			+ v['omdbapiurl'] + "\n";
+	appendToFile(myoutfile, line)
+end
+
+#printJSONseenMovie(filmsdata)
+#printJSONrateMovie(filmsdata)
+=end
+
+ 
+
+
+
   
-#print films
-#print "Found:" + films.length.to_s
